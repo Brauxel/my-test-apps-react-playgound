07f0bda31642fe02af348837cab2705e471d343b
Aakash Bhatia <Aakash.Bhatia@vicinity.com.au>
2021-08-13 21:39:54 +1000

    app running successfully

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import * as _ from 'lodash'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { App } from './App'
 
-function component() {
-  const element = document.createElement('div')
-
-  element.innerHTML = _.join(['Hello', 'webpack with TS'], ' ')
-
-  return element
-}
-
-document.body.appendChild(component())
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.body
+)
